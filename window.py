4d6d2a572b8eb5c0f88e9e11b742705ad29bc276
Artem Panchenk0 <tsi.avp@gmail.com>
2024-05-10 11:16:04 -0700

    Message Warning  For testing

--- window.py
+++ window.py
@@ -14,7 +14,7 @@ class App(tk.Tk):
 
 
     def show_error(self):
-        msg = "Warning! Your data has started to be deleted after 5 sec:)"
+        msg = "Warning message! Your data has started to be deleted after 5 sec:)"
         mb.showerror("Warning", msg)
 
 if __name__ == "__main__":
