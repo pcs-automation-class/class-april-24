5f3c7792c3b980993b45c20ef11da5f46d89e126
Artem Panchenk0 <tsi.avp@gmail.com>
2024-05-09 21:53:16 -0700

    New file for testing moving from a local PC to a remote one using PayCharm

--- window.py
+++ window.py
@@ -14,7 +14,7 @@ class App(tk.Tk):
 
 
     def show_error(self):
-        msg = "Your data has started to be deleted :)"
+        msg = "Your data has started to be deleted:)"
         mb.showerror("Warning", msg)
 
 if __name__ == "__main__":
