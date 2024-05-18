08d44c8ee1c06b433942777f40334722d31c2113
maxtester77 <pampagin@gmail.com>
2024-05-18 20:11:04 +0300

    calc code was fixed

--- calculator.py
+++ calculator.py
@@ -1,35 +1,25 @@
-# first_number = int(input("Enter first number: "))
-# second_number = int(input("Enter second number: "))
-# operation = input("Enter operation or 'q' to quit: ")
-# result = None
-#
-# # while True:
-# #     if operation == "q":
-# #         print("Exiting the calculator.")
-# #         break
-#
-# if operation == "+":
-#     result = first_number + second_number
-# elif operation == "-":
-#     result = first_number - second_number
-# elif operation == "*":
-#     result = first_number * second_number
-# elif operation == "/":
-#     if second_number != 0:
-#         result = first_number / second_number
-#     else:
-#         # TODO change message
-#         print("Error: Division by zero!")
-# else:
-#     print("Invalid operation")
-#
-# if result is not None:
-#     print("Result:", result)
+while True:
+    first_number = int(input("Enter first number: "))
+    second_number = int(input("Enter second number: "))
+    operation = input("Enter operation: ")
+    result = None
 
+    if operation == "+":
+        result = first_number + second_number
+    elif operation == "-":
+        result = first_number - second_number
+    elif operation == "*":
+        result = first_number * second_number
+    elif operation == "/":
+        if second_number != 0:
+            result = first_number / second_number
+        else:
+            print("Error: Division by zero!")
+    print(result)
 
-def my_func(a, b):
-    assert [1], "a not equal b in this test"
-
-
-my_func(1, 2)
-print("Done")
+    continue_input = input("Do you want to continue? (y/n): ")
+    if continue_input == "y":
+        continue
+    if continue_input == "n":
+        print("Quit")
+    break
