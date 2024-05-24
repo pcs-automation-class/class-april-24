398bf7e1aa47243d73dfd1f533654151b1d28250
maxtester77 <pampagin@gmail.com>
2024-05-24 21:21:32 +0300

    restored

--- environment.py
+++ environment.py
@@ -1,7 +1,6 @@
 def before_all(context):
     print("Before all")
     context.browser = "Chrome"
-    # context.browser = "Edge"
     # context.browser = "Firefox"
     # context.browser = "Opera"
 
